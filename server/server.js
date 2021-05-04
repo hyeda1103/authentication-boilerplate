696ee1fef242db21038a51a7fe159b0f0adcff7a
dahyeko <dalgona92@gmail.com>
2021-05-04 14:52:07 +0900

    Applying middlewares

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 // import routes
 const authRoutes = require("./routes/auth.js");
 
+// app middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `https://localhost:3000` }));
+}
+
 // middleware
 app.use("/api", authRoutes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`);
 });
