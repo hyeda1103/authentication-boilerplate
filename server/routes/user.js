034343b28dff09cbe4b97270e814a2ffedc699f0
dahyeko <dalgona92@gmail.com>
2021-05-23 12:00:25 +0900

    Admin middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // import controller
-const { requireSignin } = require("../controllers/auth");
+const { requireSignin, adminMiddleware } = require("../controllers/auth");
 const { read, update } = require("../controllers/user");
 
 router.get("/user/:id", requireSignin, read);
-router.put("/user/update/:id", requireSignin, update);
+router.put("/user/update", requireSignin, update);
+router.put("/admin/update", requireSignin, adminMiddleware, update);
 
 module.exports = router;
